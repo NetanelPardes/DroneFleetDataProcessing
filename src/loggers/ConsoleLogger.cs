4ae3d22c6c882b26d2be4e1b18a4e7a388882637
Ytshak_Raiyner <yr3275744@gmail.com>
2026-07-14 18:06:58 +0300

    add console logger and interface

---
+++ src/loggers/ConsoleLogger.cs
@@ -0,0 +1,16 @@
+﻿using DroneFleetDataProcessing.src.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneFleetDataProcessing.src.loggers;
+
+class ConsoleLogger : ILogger
+{
+    public void WriteLog(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
