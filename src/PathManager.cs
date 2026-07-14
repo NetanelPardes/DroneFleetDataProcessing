dc2f438dfae1d6ff2bbd7d6182556b7b8b95c238
Netanel Pardes <sendi8475@gmail.com>
2026-07-14 14:46:20 +0300

    add PathManager class

---
+++ src/PathManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneFleetDataProcessing.src
+{
+    public class PathManager
+    {
+        private string _basePath;
+
+        public PathManager()
+        {
+            _basePath = Directory.GetCurrentDirectory();
+        }
+        public string getInputRawPath(string filename)
+        {
+            return Path.Combine(_basePath,"raw", filename);
+        }
+
+        public string getInputTestScenariosPath(string filename)
+        {
+            return Path.Combine(_basePath, "test_scenarios", filename);
+        }
+
+        public string getOutputPath()
+        {
+            return (Path.Combine(_basePath, "output"));
+        }
+    }
+}
