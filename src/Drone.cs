01f2297b00c6a7091b42d868951fb7858f371b69
Ytshak_Raiyner <yr3275744@gmail.com>
2026-07-14 13:01:46 +0300

    fields of drone

---
+++ src/Drone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace DroneFleetDataProcessing.src;
+
+public class Drone
+{
+    public int Id { get; init; }
+
+    public string SerialNumber { get; init; }
+
+    public string Model { get; init; }
+
+    public string Category { get; init; }
+
+    public string Base_location { get; init; }
+    
+    public float FlightHours { get; init; }
+
+    public int BatteryHealth { get; init; }
+    
+    public float MaxRangeKm { get; init; }
+
+    public int MissionsCompleted { get; init; }
+
+    public string Status { get; init; }
+
+}
