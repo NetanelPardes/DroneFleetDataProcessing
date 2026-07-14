27dfcf3b7790b772cae52d28153344a458bbd8c6
Netanel Pardes <sendi8475@gmail.com>
2026-07-14 10:37:51 +0300

    starts a new project with yzchak

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DroneFleetDataProcessing
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Drone FleetData Processing Progect
