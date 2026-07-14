961b79d36fbdf98ab50679855011d49b9f94775c
Ytzhak <yr3275744@gmail.com>
2026-07-14 16:41:11 +0300

    Revert "Treat dron class"

--- src/Program.cs
+++ src/Program.cs
@@ -5,10 +5,8 @@ namespace DroneFleetDataProcessing.src
     {
         static void Main()
         {
-            PathManager p = new PathManager();
-            Console.WriteLine(p.getInputRawPath("drones_raw.json"));
-            Console.WriteLine(p.getInputTestScenariosPath("drones_empty.json"));
-            Console.WriteLine(p.getOutputPath());
+
+
         }
     }
 }
