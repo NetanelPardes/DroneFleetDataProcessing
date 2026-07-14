8f295ba92aedb659848c3c7c04b5362a08b0f47a
Ytshak_Raiyner <yr3275744@gmail.com>
2026-07-14 17:48:32 +0300

    remove lines from main

--- src/Program.cs
+++ src/Program.cs
@@ -6,9 +6,6 @@ namespace DroneFleetDataProcessing.src
         static void Main()
         {
             PathManager p = new PathManager();
-            Console.WriteLine(p.getInputRawPath("drones_raw.json"));
-            Console.WriteLine(p.getInputTestScenariosPath("drones_empty.json"));
-            Console.WriteLine(p.getOutputPath());
             
 
 
