17c74544289525956d1dc040dd3028bfd563539e
Ytshak_Raiyner <yr3275744@gmail.com>
2026-07-15 12:20:44 +0300

    chang names, and add summery method

--- src/SummeryDrones/SummeryDrones.cs
+++ src/SummeryDrones/SummeryDrones.cs
@@ -6,16 +6,19 @@ using System.Threading.Tasks;
 
 namespace DroneFleetDataProcessing.src.queries;
 
-public class QueryDrones
+public class SummeryDrones
 {
     List<Drone> _drones;
-    public QueryDrones(List<Drone> drones)
+    int _total;
+    public SummeryDrones(List<Drone> drones, int total)
     {
         _drones = drones;
+        _total = total;
     }
     public string GetQueries()
     {
-        return $"DRONE FLEET ANALYSIS REPORT\n\n" +
+        return $"DRONE FLEET ANALYSIS REPORT\n" +
+            $"{Summary()}" +
             $"{NonOptional()}\n" +
             $"{Top5()}\n" +
             $"{AvilableDronesNoDuplicates}\n" +
@@ -24,8 +27,27 @@ public class QueryDrones
             $"{HighestTotalCompleted()}\n" +
             $"{HighestAverageFlyHouersModels()}\n";
     }
+    private string Summary()
+    {
+        return $"PROCESSING SUMMARY\n" +
+            $"Total raw records:{_total}" +
+            $"Valid records:{_drones.Count()}" +
+            $"Rejected records:{_total - _drones.Count()}";
+    }
     private string NonOptional()
     {
+        string result = "NON-OPERATIONAL DRONES";
+        var nonOptional = _drones
+            .Where(drone => drone.Status != "Operational")
+            .Select(drone => new
+            {
+                SerialNumber = drone.SerialNumber,
+                Model = drone.Model,
+                Base = drone.Base_location,
+                Status = drone.Status
+            })
+            .ToList();
+        
 
     }
     private string Top5()
