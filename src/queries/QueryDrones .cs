2676568f3c71094e3597fe6c0a9e60ed197dc4a8
Ytshak_Raiyner <yr3275744@gmail.com>
2026-07-15 11:29:57 +0300

    add function signatures

--- src/queries/QueryDrones .cs
+++ src/queries/QueryDrones .cs
@@ -8,5 +8,37 @@ namespace DroneFleetDataProcessing.src.queries;
 
 public class QueryDrones
 {
+    List<Drone> _drones;
+    public QueryDrones(List<Drone> drones)
+    {
+        _drones = drones;
+    }
+    public string PlayQueries()
+    {
 
+    }
+    private string NonOptional()
+    {
+
+    }
+    private string Top5()
+    {
+
+    }
+    private string AvilableDronesNoDuplicates()
+    {
+
+    }
+    private string ByBase()
+    {
+
+    }
+    private string AverageBatteryHelth()
+    {
+
+    }
+    private string HighestTotalCompleted()
+    {
+
+    }
 }
